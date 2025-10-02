e233c3b3f93fbf2a22a9259d926aadf6ab066bf0
Moe Elyoussef <melyssef@umich.edu>
2025-10-02 18:56:11 -0400

    Save Hello.java as UTF-8 without BOM

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-﻿public class Hello {
+public class Hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hello Moe..." + i);
